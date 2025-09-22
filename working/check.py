6ea135508b24b7ba6eedf657826c06c988a36c84
Max Zhalilo <maxzhalilo@example.com>
2025-09-22 15:16:06 -0500

    Cleaned data to have commodity names & panels

--- working/check.py
+++ working/check.py
@@ -2,31 +2,35 @@ import pandas as pd
 
 df = pd.read_parquet("ftsfr_commodities_returns.parquet")
 
-df.to_excel("commodities_returns.xlsx", index=False)
+df = df.pivot(index='ds', columns='unique_id', values='y')
 
 gsci_indices_tickers = {
-    "SPGCBRP Index": "Brent Crude Oil",
-    "SPGCGOP Index": "Gasoil",
-    "SPGCCLP Index": "WTI Crude",
-    "SPGCHUP Index": "Unleaded Gasoline",
-    "SPGCHOP Index": "Heating Oil",
-    "SPGCNGP Index": "Natural Gas",
-    "SPGCCTP Index": "Cotton",
-    "SPGCKCP Index": "Coffee",
-    "SPGCCCP Index": "Cocoa",
-    "SPGCSBP Index": "Sugar",
-    "SPGCSOP Index": "Soybeans",
-    "SPGCKWP Index": "Kansas Wheat",
-    "SPGCCNP Index": "Corn",
-    "SPGCWHP Index": "Wheat",
-    "SPGCLHP Index": "Lean Hogs",
-    "SPGCFCP Index": "Feeder Cattle",
-    "SPGCLCP Index": "Live Cattle",
-    "SPGCGCP Index": "Gold",
-    "SPGCSIP Index": "Silver",
-    "SPGCIAP Index": "Aluminum",
-    "SPGCIKP Index": "Nickel",
-    "SPGCILP Index": "Lead",
-    "SPGCIZP Index": "Zinc",
-    "SPGCICP Index": "Copper",
+    "SPGCBRP": "Brent Crude Oil",
+    "SPGCGOP": "Gasoil",
+    "SPGCCLP": "WTI Crude",
+    "SPGCHUP": "Unleaded Gasoline",
+    "SPGCHOP": "Heating Oil",
+    "SPGCNGP": "Natural Gas",
+    "SPGCCTP": "Cotton",
+    "SPGCKCP": "Coffee",
+    "SPGCCCP": "Cocoa",
+    "SPGCSBP": "Sugar",
+    "SPGCSOP": "Soybeans",
+    "SPGCKWP": "Kansas Wheat",
+    "SPGCCNP": "Corn",
+    "SPGCWHP": "Wheat",
+    "SPGCLHP": "Lean Hogs",
+    "SPGCFCP": "Feeder Cattle",
+    "SPGCLCP": "Live Cattle",
+    "SPGCGCP": "Gold",
+    "SPGCSIP": "Silver",
+    "SPGCIAP": "Aluminum",
+    "SPGCIKP": "Nickel",
+    "SPGCILP": "Lead",
+    "SPGCIZP": "Zinc",
+    "SPGCICP": "Copper",
 }
+
+df = df.rename(columns=lambda x: gsci_indices_tickers.get(x[:7], x))
+
+print(df.head())
